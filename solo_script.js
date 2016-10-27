ccaf95e3b56dfa272df4b648504cd982571ca5e0
Luke Schlangen <lukeschlangen@gmail.com>
2016-10-27 14:26:04 -0500

    Adding bad employees

--- solo_script.js
+++ solo_script.js
@@ -5,8 +5,10 @@ var arrayAtticus = ["Atticus", "2405", "47000", 3];
 var arrayJem = ["Jem", "62347", "63500", 4];
 var arrayBoo = ["Boo", "11435", "54000", 3];
 var arrayScout = ["Scout", "6243", "74750", 5];
+var arrayRobert = ["Robert", "26835", "66000", 1];
+var arrayMayella = ["Mayella", "89068", "35000", 2];
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout, arrayRobert, arrayMayella];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
